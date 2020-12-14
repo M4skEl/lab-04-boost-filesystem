d649e8d28a749318d698d86e8b3019ef6ba223eb
M4skEl <sivereliseu7@gmail.com>
2020-12-14 23:24:45 +0300

    do cmake list && first step in iteration

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,16 +4,29 @@
 #include <boost/filesystem.hpp>
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
 namespace fs= boost::filesystem;
-int main(int argc, char* argv[]) {
+int main() {
 
-  fs::path path_to_ftp;
-  if(argc>1) path_to_ftp=argv[1];
+  fs::path path_to_ftp="../FTP";
+  /*if(argc>1) path_to_ftp=argv[1];
   else path_to_ftp=fs::current_path();
+*/
+  //if(!fs::exists(path_to_ftp))
+fs::path internal_path;
+for( const auto& iter: fs::directory_iterator{path_to_ftp}){
+ // std::cout<<iter.path()<<std::endl;
+  internal_path = iter.path();
+  cout<<internal_path.filename()<<endl;
 
-  if(!fs::exists(path_to_ftp))
+//  for(const auto & internal: fs::directory_iterator{internal_path}){
+//    std::cout<<internal.path()<<std::endl;
+//  }
+}
 
 
-  std::cout << "let's go" << std::endl;
   return 0;
 }
